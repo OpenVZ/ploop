fdc1be1b45f3ab45420cdfec989a3ffab3765fb9
Kir Kolyshkin <kir@openvz.org>
2012-04-18 03:07:46 +0400

    ploop mount usage: -d is optional
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>

--- tools/ploop.c
+++ tools/ploop.c
@@ -35,7 +35,7 @@ static struct ploop_cancel_handle *_s_cancel_handle;
 void usage_summary(void)
 {
 	fprintf(stderr, "Usage: ploop init -s SIZE [-f FORMAT] NEW_DELTA\n"
-			"       ploop mount [-rP] [-f raw] -d DEVICE BASE_DELTA [ ... TOP_DELTA ]\n"
+			"       ploop mount [-rP] [-f raw] [-d DEVICE] BASE_DELTA [ ... TOP_DELTA ]\n"
 			"       ploop umount -d DEVICE\n"
 			"       ploop { delete | rm } -d DEVICE -l LEVEL\n"
 			"       ploop merge -d DEVICE [-l LEVEL[..TOP_LEVEL]]\n"
