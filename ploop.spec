OpenVZ/ploop

commit 7309b45aa36098fe5c3001d8692f63f3ca8ad912

get_image_info(): fix a potential leak
Move ploop_get_attr("block_size") up since otherwise
find_delta_names() allocates memory for *send_from_p
and then ploop_get_attr() fails and we bail out with
SYSEXIT_SYSFS. Not a big deal, but untidy.

Reported-by: Maxim Patlasov <mpatlasov@openvz.org>
Signed-off-by: Kir Kolyshkin <kir@openvz.org>

ploop.spec
@@ -2,11 +2,11 @@
 Summary: ploop tools
 Name: ploop
 Version: 1.2
-%define rel 1
+%define rel 107.git.2d7547f
 Release: %{rel}%{?dist}
 Group: Applications/System
 License: GNU GPL
-Source: %{name}-%{version}.tar.bz2
+Source: %{name}-%{version}-%{rel}.tar.bz2
 BuildRoot: %{_tmppath}/%{name}-%{version}-root
 Requires: parted
 Requires: ploop-lib = %{version}
@@ -16,7 +16,7 @@ BuildRequires: libxml2-devel
 This package contains tools to work with ploop devices and images.
 
 %prep
-%setup -q
+%setup -n %{name}-%{version}-%{rel}
 
 %build
 make LIBDIR=%{_libdir} all

lib/pcopy.c
@@ -193,14 +193,14 @@ static int get_image_info(const char *device, char **send_from_p,
 		return SYSEXIT_SYSFS;
 	}
 
-	if (find_delta_names(device, top_level, top_level,
-				send_from_p, format_p)) {
-		ploop_err(errno, "find_delta_names");
+	if (ploop_get_attr(device, "block_size", blocksize)) {
+		ploop_err(0, "Can't find block size");
 		return SYSEXIT_SYSFS;
 	}
 
-	if (ploop_get_attr(device, "block_size", blocksize)) {
-		ploop_err(0, "Can't find block size");
+	if (find_delta_names(device, top_level, top_level,
+				send_from_p, format_p)) {
+		ploop_err(errno, "find_delta_names");
 		return SYSEXIT_SYSFS;
 	}
 

commit 0cd655c494484ae4c1b942a120fe487f1e39c33f

ploop.spec: fix
Commit 7309b45aa mistakenly commited ploop.spec modified by setver.sh
Revert it back to pristine state.

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

ploop.spec
@@ -2,11 +2,11 @@
 Summary: ploop tools
 Name: ploop
 Version: 1.2
-%define rel 107.git.2d7547f
+%define rel 1
 Release: %{rel}%{?dist}
 Group: Applications/System
 License: GNU GPL
-Source: %{name}-%{version}-%{rel}.tar.bz2
+Source: %{name}-%{version}.tar.bz2
 BuildRoot: %{_tmppath}/%{name}-%{version}-root
 Requires: parted
 Requires: ploop-lib = %{version}
@@ -16,7 +16,7 @@ BuildRequires: libxml2-devel
 This package contains tools to work with ploop devices and images.
 
 %prep
-%setup -n %{name}-%{version}-%{rel}
+%setup -q
 
 %build
 make LIBDIR=%{_libdir} all