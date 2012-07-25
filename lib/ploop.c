OpenVZ/ploop

commit 925e9f55894b0a695d3fddfa583beed04e1649f9

Introduce and use drop_sysfs_info()
Functions store_statfs_info() and read_statfs_info() were introduced to
avoid mounting/umounting ploop device in order to get used/free stats,
so statfs info is stored during ploop_umount() in .statfs file next
to the base image.

Introduce drop_sysfs_info() and call it from ploop_mount(), so now .statfs
file will only be available when ploop is unmounted.

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

lib/ploop.c
@@ -1219,6 +1219,9 @@ err:
 	if (lfd >= 0)
 		close(lfd);
 
+	if (!ret)
+		drop_statfs_info(di->images[0]->file);
+
 	return ret;
 }
 

lib/ploop.h
@@ -308,6 +308,7 @@ void get_basedir(const char *fname, char *out, int len);
 __u32 crc32(const unsigned char *buf, unsigned long len);
 int ploop_is_on_nfs(const char *path);
 int store_statfs_info(const char *mnt, char *image);
+int drop_statfs_info(const char *image);
 int read_statfs_info(const char *image, struct ploop_info *info);
 int get_statfs_info(const char *mnt, struct ploop_info *info);
 int ploop_get_child_count_by_uuid(struct ploop_disk_images_data *di, const char *guid);

lib/util.c
@@ -123,6 +123,16 @@ int read_statfs_info(const char *image, struct ploop_info *info)
 	return err;
 }
 
+int drop_statfs_info(const char *image)
+{
+	char fname[PATH_MAX];
+
+	get_basedir(image, fname, sizeof(fname)-sizeof(PLOOP_STATFS_FNAME));
+	strcat(fname, "/"PLOOP_STATFS_FNAME);
+
+	return unlink(fname);
+}
+
 int is_valid_guid(const char *guid)
 {
 	int i;

commit a468e7778639971bb4049b221d736628b77b5dd6

Fix SIGSEGV on 'ploop mount'
If ploop mount is used without DiskDescriptor.xml parameter,
a SIGSEGV occurs.

Bug introduced in commit 925e9f5.

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

lib/ploop.c
@@ -1203,7 +1203,7 @@ err:
 	if (lfd >= 0)
 		close(lfd);
 
-	if (!ret)
+	if (ret == 0 && di)
 		drop_statfs_info(di->images[0]->file);
 
 	return ret;