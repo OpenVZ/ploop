OpenVZ/ploop

commit c3b6a3bbd6f05f5c224f635822ea97a0e645017c

ploop balloon discard: add --automount
Allow to discard not mounted imaged:
* added --automount flag to allow run discard functionality on not mounted image
* changed ploop_discard API

If image not mounted it will be temporary mounted.

Reference: PSBM-12653
Reference: ploop@789175

lib/balloon.c
@@ -1071,17 +1071,57 @@ int ploop_discard_by_dev(const char *device, const char *mount_point,
 }
 
 int ploop_discard(struct ploop_disk_images_data *di,
-		__u64 minlen_b, __u64 to_free, const int *stop)
+		struct ploop_discard_param *param)
 {
 	int ret;
 	char dev[PATH_MAX];
 	char mnt[PATH_MAX];
+	int mounted = 0;
 
-	ret = ploop_get_dev_and_mnt(di, dev, sizeof(dev), mnt, sizeof(mnt));
-	if (ret)
-		return ret;
+	if (ploop_lock_di(di))
+		return SYSEXIT_LOCK;
 
-	return do_ploop_discard(di, dev, mnt, minlen_b, to_free, stop);
+	ret = ploop_find_dev(di->runtime->component_name,
+			di->images[0]->file, dev, sizeof(dev));
+	if (ret == -1) {
+		ploop_unlock_di(di);
+		return SYSEXIT_LOCK;
+	} else if (ret == 0) {
+		if (ploop_get_mnt_by_dev(dev, mnt, sizeof(mnt))) {
+			ploop_err(0, "Unable to find mount point for %s", dev);
+			ploop_unlock_di(di);
+			return SYSEXIT_PARAM;
+		}
+	} else {
+		struct ploop_mount_param mount_param = {};
+
+		if (!param->automount) {
+			ploop_err(0, "Unable to discard: image is not mounted");
+			ploop_unlock_di(di);
+			return SYSEXIT_PARAM;
+		}
+		ret = auto_mount_image(di, &mount_param);
+		if (ret) {
+			ploop_unlock_di(di);
+			return ret;
+		}
+		mounted = 1;
+		snprintf(dev, sizeof(dev), "%s", mount_param.device);
+		snprintf(mnt, sizeof(mnt), "%s", mount_param.target);
+
+		free_mount_param(&mount_param);
+	}
+	ploop_unlock_di(di);
+
+	ret = do_ploop_discard(di, dev, mnt, param->minlen_b,
+			param->to_free, param->stop);
+
+	if (mounted && ploop_lock_di(di) == 0) {
+		ploop_umount(dev, di);
+		ploop_unlock_di(di);
+	}
+
+	return ret;
 }
 
 int ploop_complete_running_operation(const char *device)

include/libploop.h
@@ -102,6 +102,14 @@ struct ploop_merge_param {
 	char dummy[32];
 };
 
+struct ploop_discard_param {
+	__u64 minlen_b;
+	__u64 to_free;
+	int automount;
+	const int *stop;
+	char dummy[32];
+};
+
 struct ploop_info {
 	unsigned long long fs_bsize;
 	unsigned long long fs_blocks;
@@ -179,7 +187,7 @@ int ploop_receive(const char *dst);
 int ploop_discard_get_stat(struct ploop_disk_images_data *di,
 		struct ploop_discard_stat *pd_stat);
 int ploop_discard(struct ploop_disk_images_data *di,
-		__u64 minlen_b, __u64 to_free, const int *stop);
+			struct ploop_discard_param *param);
 
 #ifdef __cplusplus
 }

lib/ploop.c
@@ -63,7 +63,7 @@ static int is_operation_cancelled(void)
 	return 0;
 }
 
-static void free_mount_param(struct ploop_mount_param *param)
+void free_mount_param(struct ploop_mount_param *param)
 {
 	free(param->target);
 	free(param->guid);
@@ -1229,43 +1229,18 @@ static int mount_image(struct ploop_disk_images_data *di, struct ploop_mount_par
 	return ret;
 }
 
-static int auto_mount_image(struct ploop_disk_images_data *di, struct ploop_mount_param *param, int *mounted)
+int auto_mount_image(struct ploop_disk_images_data *di,
+		struct ploop_mount_param *param)
 {
 	char mnt[PATH_MAX];
-	int ret = 0;
-	int dev_mounted = 0;
-	int fs_mounted = 0;
+	int ret;
 
-	ret = ploop_find_dev_by_uuid(di, 1, param->device, sizeof(param->device));
-	if (ret == -1)
-		return SYSEXIT_MOUNT;
-	if (ret == 0) {
-		dev_mounted = 1;
-		ret = get_mount_dir(param->device, mnt, sizeof(mnt));
-		if (ret == -1) {
-			ploop_err(0, "Can't find mount point for %s",
-				param->device);
-			return SYSEXIT_MOUNT;
-		} else if (ret == 0) {
-			param->target = strdup(mnt);
-			fs_mounted = 1;
-		}
-	}
-	if (!fs_mounted) {
-		ret = get_temp_mountpoint(di->images[0]->file, 1, mnt, sizeof(mnt));
-		if (ret)
-			return ret;
-		param->target = strdup(mnt);
-	}
+	ret = get_temp_mountpoint(di->images[0]->file, 1, mnt, sizeof(mnt));
+	if (ret)
+		return ret;
+	param->target = strdup(mnt);
 
-	if (!dev_mounted) {
-		ret = mount_image(di, param, 0);
-		if (ret == 0)
-			*mounted = 1;
-	} else if (!fs_mounted) {
-		ret = ploop_mount_fs(param);
-	}
-	return ret;
+	return mount_image(di, param, 0);
 }
 
 int ploop_mount_image(struct ploop_disk_images_data *di, struct ploop_mount_param *param)
@@ -1437,7 +1412,7 @@ int ploop_resize_image(struct ploop_disk_images_data *di, struct ploop_resize_pa
 	int ret;
 	struct ploop_mount_param mount_param = {};
 	char buf[PATH_MAX];
-	int were_mounted = 0;
+	int mounted = 0;
 	int balloonfd = -1;
 	struct stat st;
 	off_t dev_size = 0;
@@ -1456,9 +1431,10 @@ int ploop_resize_image(struct ploop_disk_images_data *di, struct ploop_resize_pa
 	if (ret == -1)
 		goto err;
 	if (ret != 0) {
-		ret = auto_mount_image(di, &mount_param, &were_mounted);
+		ret = auto_mount_image(di, &mount_param);
 		if (ret)
 			goto err;
+		mounted = 1;
 	} else {
 		ret = ploop_complete_running_operation(buf);
 		if (ret)
@@ -1568,7 +1544,7 @@ int ploop_resize_image(struct ploop_disk_images_data *di, struct ploop_resize_pa
 
 err:
 	close(balloonfd);
-	if (were_mounted)
+	if (mounted)
 		ploop_umount(mount_param.device, di);
 	ploop_unlock_di(di);
 	free_mount_param(&mount_param);

lib/ploop.h
@@ -313,6 +313,10 @@ int read_line(const char *path, char *nbuf, int len);
 int is_valid_blocksize(__u32 blocksize);
 int run_prg(char *const argv[]);
 int guidcmp(const char *p1, const char *p2);
+int auto_mount_image(struct ploop_disk_images_data *di,
+		struct ploop_mount_param *param);
+void free_mount_param(struct ploop_mount_param *param);
+
 
 // merge
 PL_EXT int get_delta_info(const char *device, struct merge_info *info);

tools/ploop-balloon.c
@@ -434,15 +434,16 @@ static int pb_discard(int argc, char **argv)
 {
 	int ret, i;
 	off_t val;
-	__u64 to_free = ~0ULL, minblock_b = 0;
 	int stat = 0;
 	static struct option long_opts[] = {
 		{ "to-free", required_argument, 0, 666 },
 		{ "min-block", required_argument, 0, 667 },
 		{ "stat", no_argument, 0, 668 },
+		{ "automount", no_argument, 0, 669 },
 		{},
 	};
 	struct ploop_disk_images_data *di = NULL;
+	struct ploop_discard_param param = {};
 
 	while ((i = getopt_long(argc, argv, "d:m:", long_opts, NULL)) != EOF) {
 		switch (i) {
@@ -458,7 +459,7 @@ static int pb_discard(int argc, char **argv)
 				usage_discard();
 				return SYSEXIT_PARAM;
 			}
-			to_free = S2B(val);
+			param.to_free = S2B(val);
 			break;
 		case 667:
 			if (parse_size(optarg, &val)) {
@@ -466,10 +467,13 @@ static int pb_discard(int argc, char **argv)
 				usage_discard();
 				return SYSEXIT_PARAM;
 			}
-			minblock_b = S2B(val);
+			param.minlen_b = S2B(val);
 			break;
 		case 668:
 			stat = 1;
+			break;
+		case 669:
+			param.automount = 1;
 			break;
 		default:
 			usage_discard();
@@ -507,9 +511,10 @@ static int pb_discard(int argc, char **argv)
 			print_discard_stat(&d_stat);
 	} else {
 		if (di)
-			ret = ploop_discard(di, minblock_b, to_free, NULL);
+			ret = ploop_discard(di, &param);
 		else
-			ret = ploop_discard_by_dev(device, mount_point, minblock_b, to_free, NULL);
+			ret = ploop_discard_by_dev(device, mount_point,
+					param.minlen_b, param.to_free, NULL);
 	}
 
 	return ret;

commit 54f85fb188ff89d1946542fe4514415391e9eae4

do_ploop_discard(): treat to_free==0 as MAX
A value of to_free=0 doesn't make sense, so let's treat it as
"free as much as possible".

Previously this was done in tools/ploop-balloon.c, but commit
c3b6a3b broke it. Rather than fixing it in tools, let's fix it
right in the library.

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

lib/balloon.c
@@ -1023,6 +1023,8 @@ static int do_ploop_discard(struct ploop_disk_images_data *di,
 	}
 	cluster = S2B(blocksize);
 
+	if (to_free == 0)
+		to_free = ~0ULL;
 	to_free = to_free  / cluster;
 	if (!to_free) {
 		ploop_err(0, "Can't shrink by less than %d bytes", cluster);