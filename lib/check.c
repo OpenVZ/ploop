b103a76dd72697fe71d9a892eba00e8f8ec412d6
Igor Sukhih <igor@openvz.org>
2013-11-12 09:27:54 +0000

    ploop: repair sparse image if flag is set
    Repair a sparse image if 'CHECK_REPAIR_SPARSE' flag is set.
    Otherwise, just check image for sparse holes.
    
    The CHECK_REPAIR_SPARSE is auto set if DiskDescriptor.xml is used.
    
    Reference: PSBM-20250
    Reference: ploop@942115

--- lib/check.c
+++ lib/check.c
@@ -31,7 +31,7 @@
 #include <linux/fiemap.h>
 
 #include "ploop.h"
-int check_and_repair_sparse(const char *image, int fd, __u64 blocksize);
+int check_and_repair_sparse(const char *image, int fd, __u64 blocksize, int flags);
 
 enum {
 	ZEROFIX = 0,
@@ -427,9 +427,8 @@ int ploop_check(char *img, int flags, int ro, int raw, int verbose, __u32 *block
 	if (!ret)
 		ret = fsync_safe(fd);
 done:
-	if (ret == 0 && (flags & CHECK_SPARSE)) {
-		ret = check_and_repair_sparse(img, fd, cluster);
-	}
+	if (ret == 0)
+		ret = check_and_repair_sparse(img, fd, cluster, flags);
 
 	ret2 = close_safe(fd);
 	if (ret2 && !ret)
@@ -442,14 +441,17 @@ done:
 	return ret;
 }
 
-static int fill_hole(const char *image, int fd, off_t start, off_t end, int *log)
+static int fill_hole(const char *image, int fd, off_t start, off_t end, int *log, int repair)
 {
 	static char buf[0x100000];
 	off_t offset;
 
 	if (!*log) {
+		ploop_err(0, "%s: ploop image '%s' is sparse.",
+				repair ? "Warning" : "Error", image);
+		if (!repair)
+			return SYSEXIT_BAD_DELTA;
 		*log = 1;
-		ploop_err(0, "Warning: ploop image '%s' is sparse.", image);
 		ploop_log(0, "Reallocating sparse blocks back.");
 	}
 
@@ -475,7 +477,7 @@ static int fill_hole(const char *image, int fd, off_t start, off_t end, int *log
 	return fsync_safe(fd);
 }
 
-int check_and_repair_sparse(const char *image, int fd, __u64 blocksize)
+int check_and_repair_sparse(const char *image, int fd, __u64 blocksize , int flags)
 {
 	int last;
 	int i, ret;
@@ -488,6 +490,7 @@ int check_and_repair_sparse(const char *image, int fd, __u64 blocksize)
 	int log = 0;
 	int count = (sizeof(buf) - sizeof(*fiemap)) /
 		    sizeof(struct fiemap_extent);
+	int repair = flags & CHECK_REPAIR_SPARSE;
 
 	ret = fstatfs(fd, &sfs);
 	if (ret < 0) {
@@ -538,7 +541,7 @@ int check_and_repair_sparse(const char *image, int fd, __u64 blocksize)
 				ploop_log(1, "Warning: the extent with unexpected flags 0x%x",
 									fm_ext[i].fe_flags);
 			if (prev_end != fm_ext[i].fe_logical &&
-					fill_hole(image, fd, prev_end, fm_ext[i].fe_logical, &log))
+					fill_hole(image, fd, prev_end, fm_ext[i].fe_logical, &log, repair))
 				return SYSEXIT_BAD_DELTA;
 
 			prev_end = fm_ext[i].fe_logical + fm_ext[i].fe_length;
@@ -546,7 +549,7 @@ int check_and_repair_sparse(const char *image, int fd, __u64 blocksize)
 	}
 
 	if (prev_end < st.st_size &&
-			fill_hole(image, fd, prev_end, st.st_size, &log))
+			fill_hole(image, fd, prev_end, st.st_size, &log, repair))
 		return SYSEXIT_BAD_DELTA;
 
 	return 0;
@@ -560,7 +563,7 @@ int check_deltas(struct ploop_disk_images_data *di, char **images,
 
 	for (i = 0; images[i] != NULL; i++) {
 		int ro;
-		int flags = CHECK_DETAILED | (di ? (CHECK_DROPINUSE | CHECK_SPARSE) : 0);
+		int flags = CHECK_DETAILED | (di ? (CHECK_DROPINUSE | CHECK_REPAIR_SPARSE) : 0);
 		__u32 cur_blocksize = 0;
 
 		ro = (images[i+1] != NULL || param->ro) ? 1 : 0;
