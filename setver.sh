5415c32c77e4412621fead93c80f27d4150c82bc
Kir Kolyshkin <kir@openvz.org>
2013-12-03 02:34:02 +0400

    setver.sh: rework buildid
    Commit 91201dd introduced buildid as a suffix after git tag to work around
    the problem of rebuilding the package from the same git head (i.e. same tag).
    The other problem is, when we do something like "git commit --amend",
    the tag changes but the release (the number after version) stays the same,
    so setver.sh -U doesn't work, with rpm complaining about a newer package
    version already installed.
    
    The solution is to separate git revision (i.e. a number of commits since
    the latest tag) from the git tag itself and only use revision to check
    if we need to add buildid.

--- setver.sh
+++ setver.sh
@@ -43,22 +43,26 @@ RPM_SPEC=${NAME}.spec
 
 # Try to figure out version from git
 GIT_DESC=$(git describe --tags | sed s'/^[^0-9]*-\([0-9].*\)$/\1/')
-GIT_V=$(echo $GIT_DESC | sed 's/-.*$//')
-				# 3.0.28-1-gf784152
-GIT_R=$(echo $GIT_DESC | sed 's/^[^-]*-\([1-9][0-9]*\)-g/\1.git./')
-test "$GIT_V" = "$GIT_R" && GIT_R="1"
+			# 3.0.28-1-gf784152
+GIT_V=$(echo $GIT_DESC | awk -F - '{print $1}')
+GIT_R=$(echo $GIT_DESC | awk -F - '{print $2}')
+GIT_T=$(echo $GIT_DESC | awk -F - '{print $3}')
+test "x$GIT_R" = "x" && GIT_R="1"
 GIT_VR="${GIT_V}-${GIT_R}"
 
 BUILDID=$(cat .build-id 2>/dev/null || echo 0)
 if test "${GIT_VR}" = "$(cat .version-id 2>/dev/null)"; then
 	BUILDID=$((BUILDID+1))
 	GIT_R="${GIT_R}.${BUILDID}"
-	GIT_VR="${GIT_VR}.${BUILDID}"
 else
 	echo "${GIT_VR}" > .version-id
 	BUILDID=0
 fi
 echo "${BUILDID}" > .build-id
+
+GIT_RT="${GIT_R}.${GIT_T}"
+test "x$GIT_T" = "x" && GIT_RT="${GIT_R}"
+GIT_VRT="${GIT_V}-${GIT_RT}"
 
 read_spec() {
 	SPEC_V=$(awk '($1 == "Version:") {print $2}' $RPM_SPEC)
@@ -79,9 +83,9 @@ if test "$GIT_VR" != "$SPEC_VR"; then
 	# Version: 3.0.28
 	# Release: 1%{?dist}
 	sed -i -e "s/^\(Version:[[:space:]]*\).*\$/\1$GIT_V/" \
-	       -e "s/^\(%define rel[[:space:]]*\).*\$/\1$GIT_R/" \
+	       -e "s/^\(%define rel[[:space:]]*\).*\$/\1$GIT_RT/" \
 		$RPM_SPEC
-	if test "$GIT_R" != "1"; then
+	if test -n "$GIT_T"; then
 		NVR='%{name}-%{version}-%{rel}'
 		sed -i -e "s/^\(Source:[[:space:]]*\).*\$/\1${NVR}.tar.bz2/" \
 		       -e "s/^%setup[[:space:]]*.*\$/%setup -n ${NVR}/" \
@@ -95,14 +99,14 @@ test "$build" = "yes" || exit 0
 make rpms || exit 1
 
 # Remove dist tarball
-test "$clean" = "yes" && rm -f ${NAME}-${GIT_VR}.tar.bz2
+test "$clean" = "yes" && rm -f ${NAME}-${GIT_VRT}.tar.bz2
 
 test -z "$install" && exit 0
 sudo rpm -${install}hv \
-	$(rpm --eval %{_rpmdir}/%{_arch})/${NAME}-*${GIT_VR}*.rpm || exit 1
+	$(rpm --eval %{_rpmdir}/%{_arch})/${NAME}-*${GIT_VRT}*.rpm || exit 1
 
 # Remove rpms
 if test "$clean" = "yes"; then
-	rm -f $(rpm --eval %{_rpmdir}/%{_arch})/${NAME}-*${GIT_VR}*.rpm
-	rm -f $(rpm --eval %{_srcrpmdir})/${NAME}-*${GIT_VR}*.src.rpm
+	rm -f $(rpm --eval %{_rpmdir}/%{_arch})/${NAME}-*${GIT_VRT}*.rpm
+	rm -f $(rpm --eval %{_srcrpmdir})/${NAME}-*${GIT_VRT}*.src.rpm
 fi
