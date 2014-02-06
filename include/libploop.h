f1bd181cb151cdee8bd470ce2f6634c1c4da7adb
Kir Kolyshkin <kir@openvz.org>
2014-02-05 17:06:14 -0800

    libploop.h: obsolete all *_diskdescriptor() calls
    The following functions are to be removed from external API:
    * ploop_store_diskdescriptor()	-> why is it in external API?
    * ploop_free_diskdescriptor()	-> use ploop_close_dd() instead
    * ploop_read_disk_descr()	-> use ploop_open_dd() instead
    
    The following functions were already removed from external API
    earlier (commit 57feac7, Sat Sep 8 03:01:34 2012):
    * ploop_alloc_diskdescriptor()
    * ploop_read_diskdescriptor()
    
    @noreview
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>

--- include/libploop.h
+++ include/libploop.h
@@ -160,12 +160,10 @@ struct ploop_discard_stat {
 extern "C" {
 #endif
 
-int ploop_read_disk_descr(struct ploop_disk_images_data **di, const char *file);
 int ploop_set_component_name(struct ploop_disk_images_data *di,
 		const char *component_name);
 char *ploop_get_base_delta_uuid(struct ploop_disk_images_data *di);
 int ploop_get_top_delta_fname(struct ploop_disk_images_data *di, char *out, int len);
-int ploop_store_diskdescriptor(const char *fname, struct ploop_disk_images_data *di);
 int ploop_find_dev(const char *component_name, const char *delta, char *buf, int size);
 int ploop_get_dev_by_mnt(const char *path, char *buf, int size);
 int ploop_get_mnt_by_dev(const char *dev, char *buf, int size);
@@ -224,7 +222,9 @@ int ploop_open_dd(struct ploop_disk_images_data **di, const char *fname);
 void ploop_close_dd(struct ploop_disk_images_data *di);
 
 /* deprecated */
+int ploop_store_diskdescriptor(const char *fname, struct ploop_disk_images_data *di);
 void ploop_free_diskdescriptor(struct ploop_disk_images_data *di);
+int ploop_read_disk_descr(struct ploop_disk_images_data **di, const char *file);
 
 #ifdef __cplusplus
 }
