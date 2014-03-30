OpenVZ/ploop

commit ea0cf1021ea8641703d15852b6212465c8bdcc52

Use component name in find device logic only on mount/umount
All ploop action performed on main ploop device (without component_name).
The only mount/umount use component_name to mount/umount snapshots.

Reference: ploop@983108

lib/ploop.c
@@ -1051,34 +1051,6 @@ int ploop_get_top_delta_fname(struct ploop_disk_images_data *di, char *out, int 
 	return 0;
 }
 
-int ploop_find_dev_by_uuid(struct ploop_disk_images_data *di,
-		int check_state, char *out, int len)
-{
-	int ret;
-	int running = 0;
-
-	if (di->nimages <= 0) {
-		ploop_err(0, "No images found in " DISKDESCRIPTOR_XML);
-		return -1;
-	}
-	ret = ploop_find_dev(di->runtime->component_name, di->images[0]->file,
-			out, len);
-	if (ret == 0 && check_state) {
-		if (ploop_get_attr(out, "running", &running)) {
-			ploop_err(0, "Can't get running attr for %s",
-					out);
-			return -1;
-		}
-		if (!running) {
-			ploop_err(0, "Unexpectedly found stopped ploop device %s",
-					out);
-			return -1;
-		}
-	}
-
-	return ret;
-}
-
 int ploop_get_dev(struct ploop_disk_images_data *di, char *out, int len)
 {
 	int ret;
@@ -1086,7 +1058,7 @@ int ploop_get_dev(struct ploop_disk_images_data *di, char *out, int len)
 	if (ploop_lock_dd(di))
 		return SYSEXIT_LOCK;
 
-	ret = ploop_find_dev(di->runtime->component_name, di->images[0]->file, out, len);
+	ret = ploop_find_dev_by_dd(di, out, len);
 
 	ploop_unlock_dd(di);
 
@@ -1297,7 +1269,7 @@ int ploop_replace_image(struct ploop_disk_images_data *di,
 		goto err;
 	}
 
-	if (ploop_find_dev_by_uuid(di, 1, dev, sizeof(dev))) {
+	if (ploop_find_dev_by_dd(di, dev, sizeof(dev))) {
 		ploop_err(0, "Can't find running ploop device");
 		goto err;
 	}
@@ -1685,7 +1657,7 @@ int ploop_mount_image(struct ploop_disk_images_data *di, struct ploop_mount_para
 	if (ploop_lock_dd(di))
 		return SYSEXIT_LOCK;
 
-	ret = ploop_find_dev_by_uuid(di, 1, dev, sizeof(dev));
+	ret = ploop_find_dev_by_cn(di, di->runtime->component_name, 1, dev, sizeof(dev));
 	if (ret == -1) {
 		ploop_unlock_dd(di);
 		return SYSEXIT_SYS;
@@ -1768,7 +1740,7 @@ int ploop_umount_image(struct ploop_disk_images_data *di)
 	if (ploop_lock_dd(di))
 		return SYSEXIT_LOCK;
 
-	ret = ploop_find_dev_by_uuid(di, 0, dev, sizeof(dev));
+	ret = ploop_find_dev_by_cn(di, di->runtime->component_name, 0, dev, sizeof(dev));
 	if (ret == -1) {
 		ploop_unlock_dd(di);
 		return SYSEXIT_SYS;
@@ -1866,7 +1838,7 @@ int get_image_param(struct ploop_disk_images_data *di, const char *guid,
 	 * get offline for non top delta.
 	 */
 	if (strcmp(di->top_guid, guid) == 0) {
-		ret = ploop_find_dev_by_uuid(di, 1, dev, sizeof(dev));
+		ret = ploop_find_dev_by_dd(di, dev, sizeof(dev));
 		if (ret == -1)
 			return SYSEXIT_SYS;
 		if (ret == 0)
@@ -1944,7 +1916,7 @@ int ploop_grow_image(struct ploop_disk_images_data *di, off_t size)
 	if (ploop_lock_dd(di))
 		return SYSEXIT_LOCK;
 
-	ret = ploop_find_dev_by_uuid(di, 1, device, sizeof(device));
+	ret = ploop_find_dev_by_dd(di, device, sizeof(device));
 	if (ret == -1) {
 		ret = SYSEXIT_SYS;
 		goto err;
@@ -2097,7 +2069,7 @@ int ploop_resize_image(struct ploop_disk_images_data *di, struct ploop_resize_pa
 	if (ploop_lock_dd(di))
 		return SYSEXIT_LOCK;
 
-	ret = ploop_find_dev_by_uuid(di, 1, buf, sizeof(buf));
+	ret = ploop_find_dev_by_dd(di, buf, sizeof(buf));
 	if (ret == -1) {
 		ret = SYSEXIT_SYS;
 		goto err;
@@ -2644,7 +2616,7 @@ int ploop_change_fmt_version(struct ploop_disk_images_data *di, int new_version,
 		goto err;
 	}
 
-	rc = ploop_find_dev_by_uuid(di, 1, fname, sizeof(fname));
+	rc = ploop_find_dev_by_dd(di, fname, sizeof(fname));
 	if (rc == -1) {
 		ret = SYSEXIT_SYS;
 		goto err;
@@ -2690,7 +2662,7 @@ int ploop_change_fmt_version(struct ploop_disk_images_data *di, int new_version,
 	}
 
 	/* Recheck ploop state after locking */
-	rc = ploop_find_dev_by_uuid(di, 1, fname, sizeof(fname));
+	rc = ploop_find_dev_by_dd(di, fname, sizeof(fname));
 	if (rc == -1) {
 		ret = SYSEXIT_SYS;
 		goto err;
@@ -2895,7 +2867,7 @@ static int ploop_get_info(struct ploop_disk_images_data *di, struct ploop_info *
 	if (ploop_lock_dd(di))
 		return SYSEXIT_LOCK;
 
-	ret = ploop_find_dev_by_uuid(di, 1, dev, sizeof(dev));
+	ret = ploop_find_dev_by_dd(di, dev, sizeof(dev));
 	if (ret == -1) {
 		ret = SYSEXIT_SYS;
 		goto err;

lib/merge.c
@@ -700,7 +700,7 @@ int ploop_merge_snapshot_by_guid(struct ploop_disk_images_data *di, const char *
 		return SYSEXIT_PARAM;
 	}
 
-	ret = ploop_find_dev_by_uuid(di, 1, dev, sizeof(dev));
+	ret = ploop_find_dev_by_dd(di, dev, sizeof(dev));
 	if (ret == -1)
 		goto err;
 	else if (ret == 0)

lib/ploop.h
@@ -324,7 +324,11 @@ int read_diskdescriptor(const char *fname, struct ploop_disk_images_data *di);
 void get_disk_descriptor_fname(struct ploop_disk_images_data *di, char *buf, int size);
 void get_disk_descriptor_lock_fname(struct ploop_disk_images_data *di, char *out, int size);
 int find_image_idx_by_guid(struct ploop_disk_images_data *di, const char *guid);
-int ploop_find_dev_by_uuid(struct ploop_disk_images_data *di, int check_state, char *out, int len);
+int ploop_find_dev(const char *component_name, const char *image, char *out, int size);
+int ploop_find_dev_by_cn(struct ploop_disk_images_data *di, const char *component_name,
+		int check_state, char *out, int len);
+int ploop_find_dev_by_dd(struct ploop_disk_images_data *di,
+		char *out, int len);
 int sys_fallocate(int fd, int mode, off_t offset, off_t len);
 int sys_syncfs(int fd);
 int create_snapshot_delta(const char *path, __u32 blocksize, off_t bdsize,
@@ -350,7 +354,6 @@ PL_EXT int find_snapshot_by_guid(struct ploop_disk_images_data *di, const char *
 int ploop_add_image_entry(struct ploop_disk_images_data *di, const char *fname, const char *guid);
 int ploop_add_snapshot_entry(struct ploop_disk_images_data *di, const char *guid,
 		const char *parent_guid, int temporary);
-int ploop_find_dev(const char *module, const char *image, char *out, int size);
 
 //balloon
 PL_EXT char *mntn2str(int mntn_type);

lib/snapshot.c
@@ -62,7 +62,7 @@ static int do_delete_snapshot(struct ploop_disk_images_data *di, const char *gui
 				guid);
 		return SYSEXIT_PARAM;
 	}
-	ret = ploop_find_dev_by_uuid(di, 1, dev, sizeof(dev));
+	ret = ploop_find_dev_by_dd(di, dev, sizeof(dev));
 	if (ret == -1)
 		return SYSEXIT_SYS;
 	else if (ret == 0 && strcmp(di->top_guid, guid) == 0) {
@@ -272,7 +272,7 @@ static int do_create_snapshot(struct ploop_disk_images_data *di,
 	if (ret)
 		return ret;
 
-	ret = ploop_find_dev_by_uuid(di, 1, dev, sizeof(dev));
+	ret = ploop_find_dev_by_dd(di, dev, sizeof(dev));
 	if (ret == -1)
 		return SYSEXIT_SYS;
 	else if (ret == 0)
@@ -570,7 +570,7 @@ int ploop_switch_snapshot_ex(struct ploop_disk_images_data *di,
 
 	if (!(flags & PLOOP_SNAP_SKIP_TOPDELTA_DESTROY)) {
 		// device should be stopped
-		ret = ploop_find_dev_by_uuid(di, 1, dev, sizeof(dev));
+		ret = ploop_find_dev_by_dd(di, dev, sizeof(dev));
 		if (ret == -1) {
 			ret = SYSEXIT_SYS;
 			goto err_cleanup1;

lib/util.c
@@ -175,6 +175,39 @@ int is_valid_guid(const char *guid)
 	return 1;
 }
 
+int ploop_find_dev_by_cn(struct ploop_disk_images_data *di,
+		const char *component_name, int check_state, char *out, int len)
+{
+	int ret;
+	int running = 0;
+
+	if (di->nimages <= 0) {
+		ploop_err(0, "No images found in " DISKDESCRIPTOR_XML);
+		return -1;
+	}
+	ret = ploop_find_dev(component_name, di->images[0]->file, out, len);
+	if (ret == 0 && check_state) {
+		if (ploop_get_attr(out, "running", &running)) {
+			ploop_err(0, "Can't get running attr for %s",
+					out);
+			return -1;
+		}
+		if (!running) {
+			ploop_err(0, "Unexpectedly found stopped ploop device %s",
+					out);
+			return -1;
+		}
+	}
+
+	return ret;
+}
+
+int ploop_find_dev_by_dd(struct ploop_disk_images_data *di,
+		char *out, int len)
+{
+	return ploop_find_dev_by_cn(di, NULL, 1, out, len);
+}
+
 int is_valid_blocksize(__u32 blocksize)
 {
 	/* 32K <= blocksize <= 64M */

commit 95c602da880bb1aa1bd8ea6768d5a510d0ca1c56

Fix vzctl snapshot-list -o device
It is broken by commit ea0cf10/ploop@983108.

Restore original ploop_get_dev() functionality,
using component_name if specified.

Reference: PSBM-25748 "vzctl snapshot-list -o device broken"
Reference: ploop@990362
Reported-by: Kir Kolyshkin <kir@openvz.org>

lib/ploop.c
@@ -1058,7 +1058,7 @@ int ploop_get_dev(struct ploop_disk_images_data *di, char *out, int len)
 	if (ploop_lock_dd(di))
 		return SYSEXIT_LOCK;
 
-	ret = ploop_find_dev_by_dd(di, out, len);
+	ret = ploop_find_dev_by_cn(di, di->runtime->component_name, 1, out, len);
 
 	ploop_unlock_dd(di);
 