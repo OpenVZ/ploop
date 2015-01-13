OpenVZ/ploop

commit 049f084cf54b141918108124d3ed62c91ed104d7

Use /proc/self/mountinfo to get mount point by device
mountinfo allow to get mount point by major:minor pair

References: ploop@838838

lib/ploop.c
@@ -799,31 +799,36 @@ static int ploop_stop(int fd, const char *devname)
 	return 0;
 }
 
-static int get_mount_dir(const char *dev, char *buf, int size)
+static int get_mount_dir(const char *device, char *out, int size)
 {
 	FILE *fp;
-	struct mntent *ent, mntbuf;
 	int ret = 1;
-	int len;
-	const char *ep;
-	char tmp[512];
+	int n;
+	char buf[PATH_MAX];
+	char target[4097];
+	unsigned _major, _minor, minor, u;
+	dev_t dev;
 
-	len = strlen(dev);
-	if (len == 0)
+	if (get_dev_by_name(device, &dev))
 		return -1;
+	minor = gnu_dev_minor(dev);
 
-	fp = fopen("/proc/mounts", "r");
+	fp = fopen("/proc/self/mountinfo", "r");
 	if (fp == NULL) {
-		ploop_err(errno, "Can't open /proc/mounts");
+		ploop_err(errno, "Can't open /proc/self/mountinfo");
 		return -1;
 	}
-	while ((ent = getmntent_r(fp, &mntbuf, tmp, sizeof(tmp)))) {
-		ep = ent->mnt_fsname + len;
+
+	while (fgets(buf, sizeof(buf), fp)) {
+		n = sscanf(buf, "%u %u %u:%u %*s %4096s", &u, &u, &_major, &_minor, target);
+		if (n != 5)
+			continue;
 		// check for /dev/ploopN or /dev/ploopNp1
-		if (strncmp(dev, ent->mnt_fsname, len) == 0 &&
-			(*ep == '\0' || strcmp(ep, "p1") == 0))
+		if (_major == PLOOP_DEV_MAJOR &&
+				(_minor == minor || _minor == minor + 1))
 		{
-			snprintf(buf, size, "%s", ent->mnt_dir);
+			strncpy(out, target, size - 1);
+			out[size - 1] = 0;
 			ret = 0;
 			break;
 		}

lib/delta_sysfs.c
@@ -178,6 +178,19 @@ static int get_dev_num(const char *path, dev_t *dev_num)
 	return 0;
 }
 
+int get_dev_by_name(const char *device, dev_t *dev)
+{
+	char nbuf[4096];
+	const char *p = device;
+
+	if ((p = strrchr(device, '/')) != NULL)
+		p++;
+
+	snprintf(nbuf, sizeof(nbuf), "/sys/block/%s/dev", p);
+
+	return get_dev_num(nbuf, dev);
+}
+
 int get_dev_start(const char *path, __u32 *start)
 {
 	char nbuf[4096];

lib/ploop.h
@@ -187,6 +187,7 @@ PL_EXT int ploop_get_attr(const char * device, const char * attr, int * res);
 int ploop_get_delta_attr(const char * device, int level, const char * attr, int * res);
 int ploop_get_size(const char * device, off_t * res);
 int dev_num2dev_start(const char *device, dev_t dev_num, __u32 *dev_start);
+int get_dev_by_name(const char *name, dev_t *dev);
 int get_dev_start(const char *path, __u32 *start);
 int ploop_get_top_level(int devfd, const char *devname, int *top);
 

commit 1fa4d1b301b79930b3b5cc5eea94f3df8cf9d603

get_mount_info(): fix for dirs with spaces etc
Problem: ploop umount is not working if there is a space in mount point.

To reproduce:
 mkdir -p some/mnt
 ploop mount DiskDescriptor.xml -m some/mnt
 mv some 'some thing'
 ploop umount DiskDescriptor.xml

Result:
 Error in get_statfs_info (util.c:44): statfs(/root/some\040thing/mnt): No such file or directory
 Unmounting file system at /root/some\040thing/mnt
 Error in do_umount (ploop.c:957): Failed to umount /root/some\040thing/mnt: No such file or directory

This problem is caused by commit 049f084 (aka r838838) since which
we are no longed using getmntent() in get_mount_info(). The thing is,
getmntent() was converting various escape sequences used to represent
spaces and tabs in file names. Now we read /proc/self/mountinfo manually
and there is no such conversion.

Fix: add proper file name conversion while reading /proc/self/mountinfo.
The code -- function unmangle_to_buffer() -- is taken from
util-linux-2.24.2 and is covered under GNU LGPL 2.1.

Signed-off-by: Kir Kolyshkin <kir@openvz.org>

lib/ploop.c
@@ -996,6 +996,40 @@ static int ploop_stop(int fd, const char *devname)
 	return 0;
 }
 
+/* Convert escape sequences used in /proc/mounts, /etc/mtab
+ * and /proc/self/mountinfo files, such as:
+ *
+ *	\040 -> space
+ *	\011 -> tab
+ *	\012 -> newline
+ *	\134 -> \
+ *
+ * Taken as is from util-linux-2.24.2, licensed under GNU LGPL 2.1
+ */
+#define isoctal(a)		(((a) & ~7) == '0')
+void unmangle_to_buffer(const char *s, char *buf, size_t len)
+{
+	size_t sz = 0;
+
+	if (!s)
+		return;
+
+	while(*s && sz < len - 1) {
+		if (*s == '\\' && sz + 3 < len - 1 && isoctal(s[1]) &&
+				isoctal(s[2]) && isoctal(s[3])) {
+
+			*buf++ = 64*(s[1] & 7) + 8*(s[2] & 7) + (s[3] & 7);
+			s += 4;
+			sz += 4;
+		} else {
+			*buf++ = *s++;
+			sz++;
+		}
+	}
+	*buf = '\0';
+}
+#undef isoctal
+
 /* Returns:
  *  0 mount point is found and saved to *out
  *  1 mount point not found (fs not mounted)
@@ -1029,8 +1063,7 @@ static int get_mount_dir(const char *device, char *out, int size)
 		if (_major == PLOOP_DEV_MAJOR &&
 				(_minor == minor || _minor == minor + 1))
 		{
-			strncpy(out, target, size - 1);
-			out[size - 1] = 0;
+			unmangle_to_buffer(target, out, size);
 			ret = 0;
 			break;
 		}