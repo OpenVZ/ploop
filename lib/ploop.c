c51df35e1cedc5472b59d06f9ac3cb61577d5aa5
Igor Sukhih <igor@parallels.com>
2016-01-27 11:11:43 +0300

    Rmove temporary mount point on ploop umount #PSBM-42493
    third attemp.
    Move temporary dir cleanup to ploop_umount() to process all cases
    
    Signed-off-by: Igor Sukhih <igor@parallels.com>

--- lib/ploop.c
+++ lib/ploop.c
@@ -2190,26 +2190,15 @@ static int ploop_stop_device(const char *device)
 static int ploop_umount_fs(const char *mnt, struct ploop_disk_images_data *di)
 {
 	int ret;
-	int clean = 0;
 
 	/* The component_name feature allows multiple image mount.
 	 * Skip store statfs in custom case.
 	 */
-	if (di != NULL) {
-		char t[PATH_MAX];
+	if (di != NULL && di->runtime->component_name == NULL)
+		store_statfs_info(mnt, di->images[0]->file);
 
-		if (di->runtime->component_name == NULL)
-			store_statfs_info(mnt, di->images[0]->file);
-
-		get_temp_mountpoint(di->images[0]->file, 0, t, sizeof(t));
-		if (strcmp(t, mnt) == 0)
-			clean = 1;
-	}
 	ploop_log(0, "Unmounting file system at %s", mnt);
 	ret = do_umount(mnt);
-
-	if (clean)
-		rmdir(mnt);
 
 	return ret;
 }
@@ -2231,6 +2220,10 @@ int ploop_umount(const char *device, struct ploop_disk_images_data *di)
 	}
 
 	ret = ploop_stop_device(device);
+
+	get_temp_mountpoint(di->images[0]->file, 0, mnt, sizeof(mnt));
+	if (access(mnt, F_OK) == 0)
+		rmdir(mnt);
 
 	return ret;
 }
@@ -2827,10 +2820,9 @@ int ploop_resize_image(struct ploop_disk_images_data *di, struct ploop_resize_pa
 err:
 	if (balloonfd != -1)
 		close(balloonfd);
-	if (mounted == 0) {
-		if (ploop_umount(mount_param.device, di) == 0)
-			rmdir(mount_param.target);
-	} else if (umount_fs)
+	if (mounted == 0)
+		ploop_umount(mount_param.device, di);
+	else if (umount_fs)
 		ploop_umount_fs(mount_param.target, di);
 
 	ploop_unlock_dd(di);
