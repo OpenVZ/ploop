155634b68efc73a5cd9842b82927cb267d70104b
Igor Sukhih <igor@virtuozzo.com>
2016-08-03 16:14:40 +0300

    LUKS disk encryption support
    call /usr/libexec/libploop/crypt.sh script to setup disk encryption.
    The crypt.sh is wrapper on 'cryptsetup <luksFormat|luksOpen|luksClose>'
    
    NOTE: there no key getter functionality it should be provided by
    palce 'getkey' binary at /usr/libexec/libploop/crypt.d
    
    Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

--- lib/crypt.c
+++ lib/crypt.c
@@ -18,37 +18,86 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "ploop.h"
 
+#define CRYPT_BIN	"/usr/libexec/ploop/crypt.sh"
+
+static const char *get_basename(const char *path)
+{
+	char *x = strrchr(path, '/');
+
+	return x ? ++x : path;
+}
+
 const char *crypt_get_device_name(const char *part, char *out, int len)
 {
-	char *x = strdupa(part);
-
-	snprintf(out, len, "%s", x);
+	snprintf(out, len, "/dev/mapper/CRYPT-%s", get_basename(part));
 
 	return out;
 }
 
+static char **get_param(const char *devname, const char *partname,
+		const char *keyid)
+{
+	char x[256];
+	int i = 0;
+	char **env = (char **)malloc(sizeof(char *)* 4);
+
+	if (env == NULL)
+		return NULL;
+
+	if (devname) {
+		snprintf(x, sizeof(x), "DEVICE=%s", devname);
+		env[i++] = strdup(x);
+	}
+
+	if (partname) {
+		snprintf(x, sizeof(x), "DEVICE_NAME=%s", get_basename(partname));
+		env[i++] = strdup(x);
+	}
+	if (keyid) {
+		snprintf(x, sizeof(x), "KEYID=%s", keyid);
+		env[i++] = strdup(x);
+	}
+	env[i] = NULL;
+
+	return env;
+
+}
+
+static int do_crypt(const char *action, const char *devname,
+		const char *partname, const char *keyid)
+{
+	int ret;
+	char *const arg[] = {CRYPT_BIN,(char *) action, NULL};
+	char **env = get_param(devname, partname, keyid);
+
+	ploop_log(0, "Crypt %s", action);
+	ret = run_prg_rc(arg, env, 0, NULL);
+
+	ploop_free_array(env);
+
+	return ret;
+}
+
 int crypt_init(const char *device, const char *keyid)
 {
-	ploop_log(0, "Crypt init");
-	return 0;
+	return do_crypt("init", device, NULL, keyid);
 }
 
 int crypt_open(const char *device, const char *part, const char *keyid)
 {
-	ploop_log(0, "Crypt open %s [%s]", part, device);
-	return 0;
+	return do_crypt("open", device, part, keyid);
 }
 
 int crypt_close(const char *part)
 {
-	ploop_log(0, "Crypt close");
-	return 0;
+	return do_crypt("close", NULL, part, NULL);
 }
 
 int crypt_resize(const char *part)
 {
-	return 0;
+	return do_crypt("resize", NULL, part, NULL);
 }
---
+++ scripts/crypt.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+export PATH="/usr/bin:/usr/sbin:/bin:/sbin"
+CRYPTSETUP=/usr/sbin/cryptsetup
+
+loadkey()
+{
+	local id=$(keyctl request2 user vdisk:$KEYID '')
+	[ -z "$id" ] && exit 1
+	KEY=`keyctl print $id | sed 's/^:hex://'`
+}
+
+init()
+{
+	loadkey
+	echo -n "$KEY" | $CRYPTSETUP luksFormat $DEVICE -
+	if [ $? -ne 0 ]; then
+		echo "Cannot format $DEVICE"
+		exit 1
+	fi
+}
+
+open()
+{
+	loadkey
+	echo -n "$KEY" | $CRYPTSETUP luksOpen $DEVICE $DEVICE_NAME
+	if [ $? -ne 0 ]; then
+		echo "Cannot open $DEVICE $DEVICE_NAME"
+		exit 1
+	fi
+}
+
+close()
+{
+	$CRYPTSETUP luksClose $DEVICE_NAME
+	if [ $? -ne 0 ]; then
+		echo "Cannot close $DEVICE_NAME"
+		exit 1
+	fi
+}
+
+resize()
+{
+	
+	$CRYPTSETUP resize $DEVICE_NAME
+	if [ $? -ne 0 ]; then
+		echo "Cannot resize $DEVICE_NAME"
+		exit 1
+	fi
+}
+
+case "$1" in
+init)
+	init
+	;;
+open)
+	open
+	;;
+resize)
+	resize
+	;;
+close)
+	close
+	;;
+*)
+	exit 1
+esac
