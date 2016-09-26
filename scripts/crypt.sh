6eb7e743c645b794acd8baa42bc6707acc041ada
Igor Sukhih <igor@virtuozzo.com>
2016-09-26 13:12:46 +0300

    revert commit e35d4a0f6
        crypt,sh: keyctl_read_alloc: Permission denied #PSBM-52358
    
        The "Permission denied" returned on dump key action.
        Add workaround by retry dump action with 30sec timeout
    
    Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

--- scripts/crypt.sh
+++ scripts/crypt.sh
@@ -12,14 +12,8 @@ loadkey()
 		exit 2
 	fi
 
-	for ((i=0; i<30; i++)); do
-		KEY=`keyctl print "$id"`
-		rc=$?
-		if [ $rc -eq 0 ]; then
-			break
-		fi
-		sleep 1
-	done
+	KEY=`keyctl print "$id"`
+	rc=$?
 	if [ $rc -ne 0 ]; then
 		echo "Cannot read the key=$KEYID id=$id rc=$rc"
 		keyctl show
