7f7b2ae5992d27a45d7fb66717cafe50e1154c87
Igor Sukhih <igor@virtuozzo.com>
2019-12-20 16:05:56 +0300

    ploop-check: added defrag option
    ploop-check -D fname
    
    Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

--- include/libploop.h
+++ include/libploop.h
@@ -376,6 +376,7 @@ int ploop_clone_dd(struct ploop_disk_images_data *di, const char *guid,
 struct ploop_bitmap *ploop_get_used_bitmap_from_image(struct ploop_disk_images_data *di, const char *guid);
 struct ploop_bitmap *ploop_get_tracking_bitmap_from_image(struct ploop_disk_images_data *di, const char *guid);
 void ploop_release_bitmap(struct ploop_bitmap *bmap);
+int ploop_image_defrag(const char *image, int flags);
 /* deprecated */
 PLOOP_DEPRECATED char *ploop_get_base_delta_uuid(struct ploop_disk_images_data *di);
 PLOOP_DEPRECATED int ploop_send(const char *device, int ofd, const char *flush_cmd, int is_pipe);
