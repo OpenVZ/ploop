b5807f9959c6a0603b76ca69c364846327ebc5f1
Igor Sukhih <igor@virtuozzo.com>
2020-04-03 16:19:13 +0300

    ploop_resize_image(): missed check
    result from get_dev_and_mnt() is not checked
    
    Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

--- lib/ploop.c
+++ lib/ploop.c
@@ -2740,8 +2740,10 @@ int ploop_resize_image(struct ploop_disk_images_data *di,
 	if (ploop_lock_dd(di))
 		return SYSEXIT_LOCK;
 
-	ret = get_dev_and_mnt(di, param->mntns_pid,  1, dev, sizeof(dev),
+	ret = get_dev_and_mnt(di, param->mntns_pid, 1, dev, sizeof(dev),
 			buf, sizeof(buf), &mounted);
+	if (ret)
+		goto err;
 	target = strdup(buf);
 
 	//FIXME: Deny resize image if there are childs
