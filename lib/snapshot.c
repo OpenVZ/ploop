0cfa98a840e6395516fa36514c0e6c3288588ab7
Igor Sukhih <igor@virtuozzo.com>
2020-04-28 14:38:10 +0300

    Unable to create snapshot with same uuid #PSBM-103419
    We will failed to create snapshot with same ID if merge was failed.
    Lets do merge temporary snapshot first before the ID check
    
    Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

--- lib/snapshot.c
+++ lib/snapshot.c
@@ -320,6 +320,8 @@ int do_create_snapshot(struct ploop_disk_images_data *di,
 	} else
 		strcpy(top_guid, TOPDELTA_UUID);
 
+	merge_temporary_snapshots(di);
+
 	if (guid != NULL) {
 		if (find_snapshot_by_guid(di, guid) != -1) {
 			ploop_err(0, "The snapshot %s already exist",
@@ -373,10 +375,6 @@ int do_create_snapshot(struct ploop_disk_images_data *di,
 		if (ret)
 			return ret;
 	}
-
-	ret = merge_temporary_snapshots(di);
-	if (ret)
-		return ret;
 
 	if (snap_dir != NULL) {
 		char *name;
