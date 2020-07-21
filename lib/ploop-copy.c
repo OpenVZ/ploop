2f90787acb2ee4942e63c2e3aa154657aca582bf
Igor Sukhih <igor@virtuozzo.com>
2020-07-21 11:07:40 +0300

    ploop_copy_receiver(): flush all modified data #PSBM-105933
    use fsync() insted of fdatasync()
    
    Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

--- lib/ploop-copy.c
+++ lib/ploop-copy.c
@@ -344,7 +344,7 @@ int ploop_copy_receiver(struct ploop_copy_receive_param *arg)
 			unsigned int cmd = ((unsigned int *) iobuf)[0];
 			switch(cmd) {
 			case PCOPY_CMD_SYNC:
-				ret = data_sync(ofd);
+				ret = fsync_safe(ofd);
 				if (ret)
 					goto out;
 				break;
@@ -371,7 +371,7 @@ int ploop_copy_receiver(struct ploop_copy_receive_param *arg)
 		}
 	}
 
-	ret = data_sync(ofd);
+	ret = fsync_safe(ofd);
 	if (ret)
 		goto out;
 
