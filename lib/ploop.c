7f606fed701b4264728bf55af7e4a3f307e61651
Igor Sukhih <igor@virtuozzo.com>
2021-12-22 14:53:18 +0300

    ploop_umount(): qcow2 live check failed #PSBM-137114
    do not pass device to unmounted image
    
    Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

--- lib/ploop.c
+++ lib/ploop.c
@@ -2671,7 +2671,7 @@ int ploop_umount(const char *device, struct ploop_disk_images_data *di)
 		}
 	}
 
-	ret = check_deltas_live(di, device);
+	ret = check_deltas_live(di, NULL);
 err:
 
 	close_delta(&d);
