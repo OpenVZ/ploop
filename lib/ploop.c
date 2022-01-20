2d799724e76a0b1e8eb71c455e30a59ef25cf960
Igor Sukhih <igor@virtuozzo.com>
2022-01-20 15:17:55 +0300

    "lazytime" mount option for both ext4 and xfs filesystems #PSBM-137413
    Put "lazytime" mount option for all ploop mounts for both ext4 and xfs filesystems.
    
    Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

--- lib/ploop.c
+++ lib/ploop.c
@@ -1399,7 +1399,7 @@ static int mount_fs(struct ploop_disk_images_data *di,
 	 * 2 remount with balloon_ino=ino
 	 */
 
-	snprintf(data, sizeof(data), "%s,%s", info.opts ?: "", param->mount_data ?: "");
+	snprintf(data, sizeof(data), "%s,%s,lazytime", info.opts ?: "", param->mount_data ?: "");
 	if (mount(partname, param->target, info.fstype, flags, data))
 		goto mnt_err;
 
