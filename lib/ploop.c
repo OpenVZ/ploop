01e17583f71f6613241fcacd1ef3f3e8542fd1bf
Aleksandr Leskin <aleksandr.leskin@virtuozzo.com>
2022-08-19 20:06:26 +0300

    ploop: do not encrypt device without encryption data or keyid #PSBM-136849
    This fix improves commit afee8aed212 (Do not treat ploop disk as encrypted
    if keyid is not set #PSBM-140446) which brings logic error in comparison.
    
    Signed-off-by: Aleksandr Leskin <aleksandr.leskin@virtuozzo.com>

--- lib/ploop.c
+++ lib/ploop.c
@@ -72,7 +72,7 @@ int get_part_devname(struct ploop_disk_images_data *di,
 	int ret, luks, gpt;
 
 	// We should only engage with encryption if keyid is set in DD.xml
-	if (!(di && di->enc && di->enc->keyid)) {
+	if (di != NULL && (di->enc == NULL || di->enc->keyid == NULL)) {
 		snprintf(devname, dlen, "%s", device);
 		return get_partition_device_name(device, partname, plen);
 	}
