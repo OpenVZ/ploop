OpenVZ/ploop

commit 95556c42474655eb2319df1a8a1b480b8ec89898

Implement merge all (-A) option, #PSBM-142653
Walk all snapshots and merge them one by one until
only top is left.

Signed-off-by: Alexander Atanasov <alexander.atanasov@virtuozzo.com>

lib/merge.c
@@ -1097,6 +1097,26 @@ int ploop_merge_snapshot(struct ploop_disk_images_data *di, struct ploop_merge_p
 	if (ploop_lock_dd(di))
 		return SYSEXIT_LOCK;
 
+	if (param->merge_all) {
+		int i;
+
+		for (i=di->nimages-2; i >= 0; i--) {
+			ploop_log(0, "merge-delete: %d guid=%s parent_guid=%s img file: %s guid: %s",
+				i, di->snapshots[i]->guid, di->snapshots[i]->parent_guid,
+				di->images[i]->file, di->images[i]->guid);
+			idx = find_snapshot_by_guid(di, di->snapshots[i]->guid);
+			if (idx == -1) {
+				ploop_err(0, "Cannot find snapshot by uuid %s", param->guid);
+				ret = SYSEXIT_PARAM;
+				goto err;
+			}
+
+			ret = ploop_delete_snapshot_by_guid(di, di->snapshots[i]->guid,
+							param->new_delta);
+		}
+
+		goto err;
+	}
 	idx = find_snapshot_by_guid(di, param->guid ? param->guid : di->top_guid);
 	if (idx == -1) {
 		ploop_err(0, "Cannot find snapshot by uuid %s", param->guid);

commit ba59bedfb11aef0964b19cb99d2347c094652276

snapshot: build fix of error [-Werror=maybe-uninitialized] #PSBM-142653
'ret' variable may be used uninitialized in cases of merge all and
without snapshot. Initialize it by default error value and append
error log for cases when it occurs.
This commit fix build broken by commit 95556c42474655eb and merged by
bc4b316359fcd869ec8.

Signed-off-by: Aleksandr Leskin <aleksandr.leskin@virtuozzo.com>

lib/merge.c
@@ -1092,7 +1092,8 @@ err:
 
 int ploop_merge_snapshot(struct ploop_disk_images_data *di, struct ploop_merge_param *param)
 {
-	int ret, idx;
+	int ret = SYSEXIT_PARAM;
+	int idx;
 
 	if (ploop_lock_dd(di))
 		return SYSEXIT_LOCK;
@@ -1114,7 +1115,8 @@ int ploop_merge_snapshot(struct ploop_disk_images_data *di, struct ploop_merge_p
 			ret = ploop_delete_snapshot_by_guid(di, di->snapshots[i]->guid,
 							param->new_delta);
 		}
-
+		if (ret == SYSEXIT_PARAM)
+			ploop_err(0, "Cannot find any snapshot by uuid %s", param->guid);
 		goto err;
 	}
 	idx = find_snapshot_by_guid(di, param->guid ? param->guid : di->top_guid);