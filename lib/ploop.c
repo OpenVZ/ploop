OpenVZ/ploop

commit f95f0a2f1345290b1aa14f53fcf684e69b85af76

statistics: introduce public function for storing statfs #PSBM-154495
Signed-off-by: Aleksandr Leskin <aleksandr.leskin@virtuozzo.com>

Conflicts:
	include/dynload.h
	lib/ploop.c

lib/ploop.c
@@ -2837,6 +2837,11 @@ static int save_cbt(struct ploop_disk_images_data *di, const char *device,
 	return 0;
 }
 
+int ploop_store_statfs_info(const char *mnt, char *image)
+{
+	return store_statfs_info(mnt, image);
+}
+
 int ploop_umount(const char *device, struct ploop_disk_images_data *di)
 {
 	int ret, last;

include/dynload.h
@@ -104,6 +104,7 @@ struct ploop_functions {
 	int (*get_names)(const char *devname, char **names[]);
 	int (*dm_message)(const char *devname, const char *msg, char **out);
 	int (*resume_device)(const char *devname);
+	int (*store_statfs_info)(const char *mnt, char *image);
 	int (*suspend_device)(const char *devname);
 	void (*free_dm_message)(char *msg);
 	int (*tg_deinit)(const char *devtg, struct ploop_tg_data *data);
@@ -111,7 +112,7 @@ struct ploop_functions {
 	int (*tg_init)(const char *dev, const char *tg, unsigned int tg_blocksize, struct ploop_tg_data *out);
 	int (*get_mnt_info)(const char *partname, struct ploop_mnt_info *info);
 	int (*compact)(struct ploop_compact_param *param);
-	void *padding[52];
+	void *padding[51];
 }; /* struct ploop_functions */
 
 __attribute__ ((visibility("default")))

include/libploop.h
@@ -361,6 +361,7 @@ int ploop_init_image(struct ploop_disk_images_data *di, struct ploop_create_para
 int ploop_init_device(const char *device, struct ploop_create_param *param);
 int ploop_mount_image(struct ploop_disk_images_data *di, struct ploop_mount_param *param);
 int ploop_mount_snapshot(struct ploop_disk_images_data *di, struct ploop_mount_param *param);
+int ploop_store_statfs_info(const char *mnt, char *image);
 int ploop_umount(const char *device, struct ploop_disk_images_data *di);
 int ploop_umount_image(struct ploop_disk_images_data *di);
 int ploop_replace_image(struct ploop_disk_images_data *di, struct ploop_replace_param *param);

commit 05b3cdb5c0ab09a66c1149077df9905a668c77b3

statistics: check if there are images before updating stats, #VSTOR-84962
We can work without having DiskDescriptor.xml or it can
be empty, i.e. not having any images in it. This results in crash
when trying to store statistics.

Fix this by properly checking for both cases - have a di and have images.

Fixes: f95f0a2f1345 ("statistics: introduce public function for storing statfs #PSBM-154495")
https://pmc.acronis.work/browse/VSTOR-84962
Signed-off-by: Alexander Atanasov <alexander.atanasov@virtuozzo.com>

lib/ploop.c
@@ -2844,7 +2844,7 @@ static int ploop_umount_fs(const char *mnt, struct ploop_disk_images_data *di)
 	/* The component_name feature allows multiple image mount.
 	 * Skip store statfs in custom case.
 	 */
-	if (di != NULL && di->runtime->component_name == NULL)
+	if (di != NULL && di->images != NULL && di->runtime->component_name == NULL)
 		store_statfs_info(mnt, di->images[0]->file);
 
 	ploop_log(0, "Unmounting file system at %s", mnt);