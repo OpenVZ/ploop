OpenVZ/ploop

commit 742b0c2614f319e7dd7709ab0d170fa2ce43fa22

ploop: check qcow2 images on device umount, #PSBM-152511
Implement a check of images when umounting a qcow2 backed device.
Fix order of methods in dynload.h - they need to be at the
end of the struct.

https://virtuozzo.atlassian.net/browse/PSBM-152511
Signed-off-by: Alexander Atanasov <alexander.atanasov@virtuozzo.com>

lib/di.c
@@ -503,6 +503,48 @@ err:
 	return rc;
 }
 
+int ploop_make_dd_from_device(struct ploop_disk_images_data **di, const char *device)
+{
+	int i, rc;
+	char **images;
+	struct ploop_disk_images_data *p;
+
+	rc = ploop_get_names(device, &images);
+	if (rc != 0 || !*images) {
+		fprintf(stderr, "Failed to get image names: %s\n", device);
+		return 1;
+	}
+
+	*di = NULL;
+	p = alloc_diskdescriptor();
+	if (p == NULL) {
+		rc = SYSEXIT_MALLOC;
+		goto out_err;
+	}
+
+	i = 0;
+	p->runtime->xml_fname = strdup(images[i]);
+
+	while(images[i]) {
+		rc = ploop_dd_add_image(p, images[i]);
+		if (rc) {
+			fprintf(stderr, "Failed to add image:%s\n", images[i]);
+			ploop_free_array(images);
+			goto out_err;
+		}
+		i++;
+	}
+
+	*di = p;
+	rc = 0;
+out_err:
+	ploop_free_array(images);
+	return rc;
+}
+
+/*
+ * Caller must provide a list of images of the same format QCOW2
+ */
 int ploop_make_dd_from_imgs(struct ploop_disk_images_data **di, char **imgs)
 {
 	const char *img = *imgs;

include/dynload.h
@@ -76,7 +76,6 @@ struct ploop_functions {
 	void (*close_dd)(struct ploop_disk_images_data *di);
 	int (*create_temporary_snapshot)(struct ploop_disk_images_data *di, struct ploop_tsnapshot_param *param, int *holder_fd);
 	int (*is_mounted)(struct ploop_disk_images_data *di);
-	int (*make_dd_from_imgs)(struct ploop_disk_images_data **di, char **imgs);
 	/* 1.12 */
 	int (*copy_receiver)(struct ploop_copy_receive_param *arg);
 	int (*get_max_size)(unsigned int blocksize, unsigned long long *max);
@@ -113,6 +112,8 @@ struct ploop_functions {
 	int (*tg_init)(const char *dev, const char *tg, unsigned int tg_blocksize, struct ploop_tg_data *out);
 	int (*get_mnt_info)(const char *partname, struct ploop_mnt_info *info);
 	int (*compact)(struct ploop_compact_param *param);
+	int (*make_dd_from_imgs)(struct ploop_disk_images_data **di, char **imgs);
+	int (*make_dd_from_device)(struct ploop_disk_images_data **di, const char *device);
 	void *padding[51];
 }; /* struct ploop_functions */
 

include/libploop.h
@@ -412,6 +412,7 @@ int ploop_discard(struct ploop_disk_images_data *di,
 int ploop_compact(struct ploop_compact_param *param);
 int ploop_open_dd(struct ploop_disk_images_data **di, const char *fname);
 int ploop_make_dd_from_imgs(struct ploop_disk_images_data **di, char **imgs);
+int ploop_make_dd_from_device(struct ploop_disk_images_data **di, const char *device);
 void ploop_close_dd(struct ploop_disk_images_data *di);
 int ploop_create_dd(const char *ddxml, struct ploop_create_param *param);
 int ploop_read_dd(struct ploop_disk_images_data *di);

lib/ploop.c
@@ -2886,6 +2886,8 @@ int ploop_umount(const char *device, struct ploop_disk_images_data *di)
 	struct delta d = {.fd = -1};
 	struct ploop_pvd_header *vh;
 	int image_fmt;
+	struct ploop_disk_images_data *ldi = NULL;
+	int is_device_mapper = ploop_is_devicemapper();
 
 	if (!device) {
 		ploop_err(0, "ploop_umount: device is not specified");
@@ -2893,56 +2895,66 @@ int ploop_umount(const char *device, struct ploop_disk_images_data *di)
 	}
 
 	ploop_log(0, "Umount %s", device);
+	ldi = di;
+	if (is_device_mapper && !ldi) {
+		ret = ploop_make_dd_from_device(&ldi, device);
+		if (ret) {
+			ploop_err(0, "failed to create ddxml from device");
+			return SYSEXIT_MALLOC;
+		}
+	}
 	ret = get_part_devname_from_sys(device, devname, sizeof(devname),
 			partname, sizeof(partname));
 	if (ret)
-		return ret;
+		goto out_frdd;
 
 	if (get_mount_dir(partname, 0, mnt, sizeof(mnt)) == 0) {
-		ret = ploop_umount_fs(mnt, di);
+		ret = ploop_umount_fs(mnt, ldi);
 		if (ret)
-			return ret;
+			goto out_frdd;
 	}
 
 	if (get_crypt_layout(devname, partname)) {
 		ret = crypt_close(devname, partname);
 		if (ret)
-			return ret;
+			goto out_frdd;
 	}
 
 	do {
 		ret = get_part_devname_from_sys(device, devname, sizeof(devname),
 				partname, sizeof(partname));
 		if (ret)
-			return ret;
+			goto out_frdd;
 
 		last = strcmp(devname, partname) == 0;
 		if (!last) {
 			ret = dm_remove(partname, get_umount_tm(di));
 			if (ret)
-				return ret;
+				goto out_frdd;
 		}
 	} while (!last);
 
-	ret = get_image_param_online(di, device, &top, NULL, NULL,
+	ret = get_image_param_online(ldi, device, &top, NULL, NULL,
 			&fmt, &image_fmt);
 	if (ret)
-		return ret;
+		goto out_frdd;
+
+	ldi->runtime->image_fmt = image_fmt;
 
 	if (image_fmt == PLOOP_FMT) {
 		if (open_delta(&d, top, O_RDWR, OD_ALLOW_DIRTY) == 0) {
-			ret = save_cbt(di, device, &d);
+			ret = save_cbt(ldi, device, &d);
 			if (ret)
 				goto err;
 		}
 	} else if (image_fmt == QCOW_FMT) {
-		ret = qcow_umount(di, device, top);
+		ret = qcow_umount(ldi, device, top);
 		if (ret)
 			goto err;
 	}
 
 	cn_find_name(device, cn, sizeof(cn), 1);
-	ret = ploop_stop(device, di);
+	ret = ploop_stop(device, ldi);
 	if (ret)
 		goto err;
 
@@ -2951,8 +2963,8 @@ int ploop_umount(const char *device, struct ploop_disk_images_data *di)
 		unlink(cn);
 	}
 
-	if (di != NULL) {
-		get_temp_mountpoint(di->images[0]->file, 0, mnt, sizeof(mnt));
+	if (ldi != NULL) {
+		get_temp_mountpoint(ldi->images[0]->file, 0, mnt, sizeof(mnt));
 		if (access(mnt, F_OK) == 0)
 			rmdir(mnt);
 	}
@@ -2966,11 +2978,14 @@ int ploop_umount(const char *device, struct ploop_disk_images_data *di)
 		}
 	}
 
-	ret = check_deltas_live(di, NULL);
+	ret = check_deltas_live(ldi, NULL);
 err:
 
 	close_delta(&d);
 	free(top);
+out_frdd:
+	if (ldi && !di)
+		ploop_free_diskdescriptor(ldi);
 
 	return ret;
 }

lib/ploop.h
@@ -356,6 +356,7 @@ int normalize_image_name(const char *basedir, const char *image, char *out, int 
 int PWRITE(struct delta * delta, void * buf, unsigned int size, off_t off);
 int PREAD(struct delta * delta, void *buf, unsigned int size, off_t off);
 struct ploop_disk_images_data *alloc_diskdescriptor(void);
+void ploop_free_diskdescriptor(struct ploop_disk_images_data *di);
 int ploop_store_diskdescriptor(const char *fname, struct ploop_disk_images_data *di);
 PL_EXT int ploop_read_disk_descr(struct ploop_disk_images_data **di, const char *file);
 void get_disk_descriptor_fname(struct ploop_disk_images_data *di, char *buf, int size);

lib/qcow.c
@@ -400,9 +400,9 @@ int qcow_check(struct ploop_disk_images_data *di)
 int qcow_live_check(const char *device)
 {
 	int rc;
-	char *top;
+	struct ploop_disk_images_data *di;
 
-	rc = get_image_param_online(NULL, device, &top, NULL, NULL, NULL, NULL);
+	rc = ploop_make_dd_from_device(&di, device);
 	if (rc)
 		return rc;
 
@@ -410,11 +410,11 @@ int qcow_live_check(const char *device)
 	if (rc)
 		goto err;
 
-	rc = do_qcow_check(top);
+	rc = qcow_check(di);
 	ploop_resume_device(device);
 
 err:
-	free(top);
+	ploop_free_diskdescriptor(di);
 	return rc;
 }
 
@@ -1013,8 +1013,10 @@ int qcow_umount(struct ploop_disk_images_data *di,
 	rc = cbt_get_dirty_bitmap_metadata(fd, uuid, NULL);
 	close(fd);
 	if (rc) {
-		if (rc == SYSEXIT_NOCBT)
+		if (rc == SYSEXIT_NOCBT) {
+			ploop_log(0, "Clean no dirty bitmap");
 			rc = 0;
+		}
 		goto err;
 	}
 

commit b1a4101ee81ce3c1b4dabe19f21642e392802c84

ploop: properly create ddxml from device for ploop devices, #VSTOR-85027
Currently when creating ddxml from device only qcow2 was supported
which resulted in crash when provided with ploop backed device.
Improve that so when working with ploop devices to construct valid
representation of ddxml inside tool.

Fixes: 742b0c2614f3 ("ploop: check qcow2 images on device umount, #PSBM-152511")
https://pmc.acronis.work/browse/VSTOR-85027
Signed-off-by: Alexander Atanasov <alexander.atanasov@virtuozzo.com>

lib/di.c
@@ -462,6 +462,7 @@ int ploop_dd_add_image(struct ploop_disk_images_data *di, const char *fname)
 	int rc, image_fmt = -1;
 	char path[PATH_MAX];
 	struct stat st;
+	int is_ddxml = 0;
 
 	if (stat(fname, &st)) {
 		ploop_err(errno, "Can't open %s", fname);
@@ -481,10 +482,13 @@ int ploop_dd_add_image(struct ploop_disk_images_data *di, const char *fname)
 	if (S_ISDIR(st.st_mode)) {
 		int pathLength = strlen(path);
 		strcpy(path + pathLength, "/"DISKDESCRIPTOR_XML);
-		if (!access(path, F_OK))
+		if (!access(path, F_OK)) {
 			image_fmt = PLOOP_FMT;
+			is_ddxml = 1;
+		}
 	} else if (strcmp(get_basename(path), DISKDESCRIPTOR_XML) == 0) {
 		image_fmt = PLOOP_FMT;
+		is_ddxml = 1;
 	} else {
 		rc = detect_image_fmt(path, &image_fmt);
 		if (rc)
@@ -498,7 +502,18 @@ handle_qcow2:
 			goto err;
 	}
 
-	return 0;
+	rc = 0;
+	if (!is_ddxml && image_fmt == PLOOP_FMT) {
+		const char *guid = TOPDELTA_UUID;
+		const char *parent_guid = NONE_UUID;
+		char gtmp[256];
+		// need to fake valid guid here to pass the validation in make_images_list
+		snprintf(gtmp, sizeof(gtmp), "{5fbaabe3-CAFE-B03A-4A4A-4A0e329aab%02X}", (unsigned)di->nimages);
+		if (di->nimages > 0)
+			parent_guid = di->images[di->nimages-1]->guid;
+		rc = ploop_di_add_image(di, fname, guid, parent_guid);
+	}
+
 err:
 	return rc;
 }
@@ -535,6 +550,10 @@ int ploop_make_dd_from_device(struct ploop_disk_images_data **di, const char *de
 		i++;
 	}
 
+	if (!p->images) {
+		rc = SYSEXIT_MALLOC;
+		goto out_err;
+	}
 	*di = p;
 	rc = 0;
 out_err: